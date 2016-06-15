94e112b41ba3d141709471af65e2fa850e26adc0
mala <mala235813@gmail.com>
2016-06-15 20:28:43 +0200

    Initial web app

---
+++ src/cljs/modern_cljs/core.cljs
@@ -0,0 +1,10 @@
+(ns modern-cljs.core)
+
+;; create the main project namespace
+(ns modern-cljs.core)
+
+;; enable cljs to print to the JS console of the browser
+(enable-console-print!)
+
+;; print to the console
+(println "Hello, World!")
